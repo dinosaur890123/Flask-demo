ce2d769b48a4ea3151f55a6e21bd11c40f6861c0
dinosaur890123 <dinosaur890123@gmail.com>
2025-08-05 12:01:22 +1000

    Make more advanced

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 app = Flask(__name__)
 todos = []
+DATA_FILE ='todos.json'
 @app.route('/')
+def load_todos():
+    try:
+        with open(DATA_FILE, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        default_todos = ["Do this", "Do that", "Do something"]
+        save_todos(default_todos)
+        return default_todos
+def save_todos(todos):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(todos, f)
+
+
 def home():
+    todos = load_todos()
     return render_template('index.html', todos=todos)
 
-@app.route('/add', methods=['POST'])
 
+@app.route('/add', methods=['POST'])
 def add_task():
     new_task = request.form.get('task_name')
     if new_task:
         todos.append(new_task)
     return redirect(url_for('home'))
+
 
 @app.route('/delete/<int:task_index>')
 def delete_task(task_index):
